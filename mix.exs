a99c8e8c7257221ad47bc29f000b1bb5fe03d5ac
Chirag Rajkarnikar <chiragrajk@gmail.com>
2020-04-03 14:49:39 +0200

    update to elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,8 +5,10 @@ defmodule SNSVerifier.MixProject do
     [
       app: :sns_verifier,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: "Verifies message from AWS SNS",
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,4 +25,20 @@ defmodule SNSVerifier.MixProject do
       {:mox, "~> 0.5", only: :test}
     ]
   end
+
+  defp package do
+    [
+      maintainers: [
+        "7Mind GmbH",
+        "Chirag Rajkarnikar"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => github_link(),
+        "Verifying the signatures of Amazon SNS messages" => "https://docs.aws.amazon.com/sns/latest/dg/sns-verify-signature-of-message.html"
+      }
+    ]
+  end
+
+  defp github_link, do: "https://github.com/sevenmind/sns_verifier"
 end
